73622dc1736ae5f41f95c25e932d95da8838fd37
Kaushik G Bhat <kaushikgbhat@Kaushiks-Mac-mini.local>
2023-01-29 11:45:29 +0530

    responsive remaining of carousel

--- components/Hero-section/Hero.jsx
+++ components/Hero-section/Hero.jsx
@@ -1,12 +1,14 @@
 'use client'
 import "./Hero.scss";
 import HeroImage from "../../public/HeroImage.webp";
+import HeroMobileImage  from "../../public/hero-mobile-img.webp"
 import Image from "next/image";
 import { Button } from "@mui/material";
 const Hero = () => {
   return (
     <main className="hero">
-      <Image src={HeroImage} className="HeroImg" alt="Hero img" />
+      <Image src={HeroImage} className="hero__Img img" alt="Hero img" />
+      <Image src={HeroMobileImage} className="hero__Img__Mobile img" alt="Hero img" />
       <div className="hero__content">
         <h2 className="hero__heading">HERO11</h2>
         <h1 className="hero__subHeading">Kick the new year into gear. Extra 15% Off Sitewide.</h1>
